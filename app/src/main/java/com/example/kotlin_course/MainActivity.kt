14d456309ad80b63a624b437133987d1b15d98e4
Phant0m-r <faizulin_adil@mail.ru>
2021-12-06 23:54:28 +0600

    feat: add second comment

--- app/src/main/java/com/example/kotlin_course/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_course/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //comment to check git
+        //second comment to check branch
     }
 }
